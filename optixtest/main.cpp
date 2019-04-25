18ae3b0d7aeb131eccfea5acaa97b420b3c041f5
gzfrozen <gzliuxiaochen@gmail.com>
2019-04-25 22:52:12 +0900

    Code works fine, however no GUI window.

--- optixtest/main.cpp
+++ optixtest/main.cpp
@@ -1,7 +1,38 @@
-#include <cstdio>
-
+#include <iostream>
+#include <GLFW/glfw3.h>
 int main()
 {
-    printf("hello from optixtest!\n");
-    return 0;
+	// GLFW を初期化する
+	if (glfwInit() == GL_FALSE)
+	{
+		// 初期化に失敗した
+		std::cerr << "Can't initialize GLFW" << std::endl;
+		return 1;
+	}
+	// ウィンドウを作成する
+	GLFWwindow* const window(glfwCreateWindow(640, 480, "Hello!", NULL, NULL));
+	if (window == NULL)
+	{
+		// ウィンドウが作成できなかった
+		std::cerr << "Can't create GLFW window." << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+	// 作成したウィンドウを OpenGL の処理対象にする
+	glfwMakeContextCurrent(window);
+	// 背景色を指定する
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+	// ウィンドウが開いている間繰り返す
+	while (glfwWindowShouldClose(window) == GL_FALSE)
+	{
+		// ウィンドウを消去する
+		glClear(GL_COLOR_BUFFER_BIT);
+		//
+		// ここで描画処理を行う
+		//
+		// カラーバッファを入れ替える
+		glfwSwapBuffers(window);
+		// イベントを取り出す
+		glfwWaitEvents();
+	}
 }
